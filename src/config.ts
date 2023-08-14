3c79782a46f3e8a1b737c3a46e3275d96ebc1bd9
shellGrace <2420082764@qq.com>
2023-08-15 01:08:09 +0800

    feat: 更新博客地址、

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const SITE = {
   icon: 'https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png',
   pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
   homePage: 'https://weeklyFri.shellgrace.fun',
-  blogPage: 'https://shellGrace.github.io',
+  blogPage: 'https://github.com/shellGrace/shellGrace.github.io',
   twitterId: 'shellGrace',
   githubId: 'shellgrace',
   repo: 'shellgrace/weeklyFri'
